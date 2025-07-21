7be4dc90f1db13d08ec90a42c4db73dd94d078f2
Aneesh Kumar <aneeshkumar.klr@gmail.com>
2025-07-21 01:55:05 +0100

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '@/amplify_outputs.json';  // Adjust path if needed
+import '@aws-amplify/ui-react/styles.css';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+Amplify.configure(outputs);
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default withAuthenticator(App);
